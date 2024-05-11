c618d181459140be45454eabeba9d1e8039f841b
prsanjeet12 <prsanjeet.p@zeddlabz.com>
2024-05-11 09:57:03 +0530

    new

--- src/features/Navigation/Navigation.tsx
+++ src/features/Navigation/Navigation.tsx
@@ -16,67 +16,68 @@ import AccountUpdate from '../components/AccountUpdate';
 import UserLogsTable from '../components/UserLogs';
 import CarManagement from '../Tables/WareHouseManagment/CarManagment';
 import DriverCarApproval from '../Tables/WareHouseManagment/DriverCarApprovals';
+
 const Navigation: React.FC = () => {
   const [open, setOpen] = useState(true);
-  const [authenticated, setAuthenticated] = useState(false);
+  // const [authenticated, setAuthenticated] = useState(false);
 
-
-  const handleLogin = () => {
-    setAuthenticated(true);
-  };
+  // const handleLogin = () => {
+  //   setAuthenticated(true);
+  // };
 
   return (
     <Router>
       <div className="flex bg-[#e6e8f2] h-screen">
      
-        {authenticated && (
-          <div
-            className={`
-              ${open ? "w-52" : "w-20"}
-              duration-300
-              p-5 pt-8
-              h-screen
-              relative 
-              bg-gradient-to-b
-              from-gray-900
-              to-gray-950`}
-          >
-            <Sidebar setOpen={setOpen} open={open} />
-          </div>
-        )}
+        {/* {authenticated && ( */}
+        <div
+          className={`
+            ${open ? "w-52" : "w-20"}
+            duration-300
+            p-5 pt-8
+            h-screen
+            relative 
+            bg-gradient-to-b
+            from-gray-900
+            to-gray-950`}
+        >
+          <Sidebar setOpen={setOpen} open={open} />
+        </div>
+        {/* )} */}
         <div className="h-full w-screen">
       
-          {authenticated && <Header />}
+          {/* {authenticated && <Header />} */}
+          <Header />
           <Routes>
         
-            {authenticated ? (
-              <>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/driver-table" element={<DriverTable />} />
-                <Route path="/rider-table" element={<RiderTable />} />
-                <Route path="/admin-table" element={<AdminTable />} />
-                <Route path="/driver-sercharges" element={<DriverSerchargeTable />} />
-                <Route path="/driver-sercharges-map" element={<DriverSurchargeMapTable />} />
-                <Route path='/api' element={<ApiTable/>}/>
-                <Route path='/notifications' element={<NotificationTable notifications={[]} />} />
-                <Route path='/accountUpdate' element={<AccountUpdate/>}/>
-                <Route path='/user-logs' element={<UserLogsTable/>}/>
-                <Route path='/CarManagement' element={<CarManagement/>}/>
-                <Route path='/driver-Requests' element={<DriverCarApproval/>}/>
+            {/* {authenticated ? ( */}
+            <>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/driver-table" element={<DriverTable />} />
+              <Route path="/rider-table" element={<RiderTable />} />
+              <Route path="/admin-table" element={<AdminTable />} />
+              <Route path="/driver-sercharges" element={<DriverSerchargeTable />} />
+              <Route path="/driver-sercharges-map" element={<DriverSurchargeMapTable />} />
+              <Route path='/api' element={<ApiTable/>}/>
+              <Route path='/notifications' element={<NotificationTable notifications={[]} />} />
+              <Route path='/accountUpdate' element={<AccountUpdate/>}/>
+              <Route path='/user-logs' element={<UserLogsTable/>}/>
+              <Route path='/CarManagement' element={<CarManagement/>}/>
+              <Route path='/driver-Requests' element={<DriverCarApproval/>}/>
 
-              </>
-            ) : (
+            </>
+            {/* ) : ( */}
               <Route
                 path="/login"
                 element={<Login  />} 
               />
-            )}
+            {/* )} */}
             {/* onLogin={handleLogin} */}
           
-            <Route
+            {/* <Route
               path="*"
               element={authenticated ? <Navigate to="/" /> : <Navigate to="/login" />}
-            />
+            /> */}
           </Routes>
         </div>
       </div>
